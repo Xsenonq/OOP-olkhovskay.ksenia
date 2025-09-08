0a51e455e1b535cf495b0f4796b8ac139e09d77c
Xsenonq <olkhovska.k_ipz23@rcit.ukr.education>
2025-09-09 00:17:42 +0300

    Lab1: Added Figure class

---
+++ lab1nv12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Song
+{
+
+    private string title;
+    private string artist;
+
+
+    public double Duration { get; set; }
+
+
+    public Song(string title, string artist, double duration)
+    {
+        this.title = title;
+        this.artist = artist;
+        Duration = duration;
+    }
+
+
+    public void Play()
+    {
+        Console.WriteLine($"Грає пісня: {title} — {artist} ({Duration} хв.)");
+    }
+
+
+    ~Song()
+    {
+        Console.WriteLine($"Об'єкт пісні {title} видалено з пам'яті.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Song s1 = new Song("Syren", "Anyma", 3.1);
+        Song s2 = new Song("Without Me", "Eminem", 4.5);
+        Song s3 = new Song("Coming Undone", "Korn", 3.2);
+
+
+        s1.Play();
+        s2.Play();
+        s3.Play();
+
+        Console.WriteLine(" Усі піснi відтворені.");
+    }
+}
