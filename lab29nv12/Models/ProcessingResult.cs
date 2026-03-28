300fa757586ff3c1c5163a1feb9791dafceddfa5
Xsessha <olkhovskayxseniia@gmail.com>
2026-03-28 18:41:46 +0200

    Lab 29

---
+++ lab29nv12/Models/ProcessingResult.cs
@@ -0,0 +1,9 @@
+namespace lab29nv12.Models
+{
+    public class ProcessingResult
+    {
+        public long Time { get; set; }
+        public int SpamCount { get; set; }
+        public int NormalCount { get; set; }
+    }
+}
---
+++ lab29nv12/Program.cs
@@ -0,0 +1,31 @@
+﻿using lab29nv12.Services;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string input = "messages.csv";
+        string output = "filtered.csv";
+
+        int lines = 1_000_000;
+
+        FileGenerator generator = new FileGenerator();
+        FileProcessor processor = new FileProcessor();
+
+        Console.WriteLine(" Генерація файлу...");
+        await generator.GenerateAsync(input, lines);
+
+        Console.WriteLine(" Асинхронна обробка...");
+        var asyncResult = await processor.ProcessAsync(input, output);
+
+        Console.WriteLine(" Синхронна обробка...");
+        var syncResult = processor.ProcessSync(input);
+
+        Console.WriteLine("\n РЕЗУЛЬТАТИ:");
+        Console.WriteLine($"Async час: {asyncResult.Time} ms");
+        Console.WriteLine($"Sync час: {syncResult.Time} ms");
+
+        Console.WriteLine($"\nSpam: {asyncResult.SpamCount}");
+        Console.WriteLine($"Normal: {asyncResult.NormalCount}");
+    }
+}
